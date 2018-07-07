6555d5b2c98f12d2f410752ef0743f85578276d0
xiaofd <568032134@qq.com>
2018-07-07 17:22:04 +0800

    Create vncff.sh

---
+++ vncff.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update && apt-get install -y sudo
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo apt-get update
+sudo apt-get install -y wget xorg lxde-core tightvncserver flashplugin-installer
+
+mkdir -p ~/.vnc
+wget xiaofd.github.io/others/passwd -P ~/.vnc/
+chmod 0400 ~/.vnc/passwd
+wget xiaofd.github.io/others/xstartup -P ~/.vnc/
+chmod +x ~/.vnc/xstartup
+
+export USER=~
+tightvncserver :1
+
+sudo apt-get install firefox
