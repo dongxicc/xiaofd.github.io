aa6f8d20ad105bf631ed51d2a5e396f8c65514e0
xiaofd <568032134@qq.com>
2018-07-08 17:22:35 +0800

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#set default keyboard layout
+#防止安装xorg时，可能要求选择键盘布局
+cat > /etc/default/keyboard <<EOF
+XKBMODEL="pc105"
+XKBLAYOUT="us"
+XKBVARIANT=""
+XKBOPTIONS=""
+BACKSPACE="guess"
+EOF
+
 apt-get update && apt-get install -y sudo
 sudo apt-get update
 sudo apt-get install -y software-properties-common
