3be5a742b1ffe063326e336d445af4cba4faa4af
xiaofd <568032134@qq.com>
2018-07-09 13:23:10 +0800

    Update cron-reboot.sh

--- cron-reboot.sh
+++ cron-reboot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #every 2 hours
-apt-get update
-apt-get install sudo
+apt-get update && apt-get install -y sudo
 sudo chmod 0777 /etc/cron.d
 sudo cat > /etc/cron.d/cron-reboot << EOF
 * */2 * * * root reboot
