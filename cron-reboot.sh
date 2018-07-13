e4e276ef50fa6a89b3d5e4a3088463c8577ffe1e
xiaofd <568032134@qq.com>
2018-07-13 08:23:45 +0800

    Update cron-reboot.sh

--- cron-reboot.sh
+++ cron-reboot.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get install -y sudo
 sudo chmod 0777 /etc/cron.d
 sudo cat > /etc/cron.d/zz-cron-reboot << EOF
-* */2 * * * root /sbin/shutdown -r now
+0 */2 * * * root /sbin/shutdown -r now
 EOF
 sudo chmod 0644 /etc/cron.d/zz-cron-reboot
 sudo chown root:root /etc/cron.d/zz-cron-reboot
