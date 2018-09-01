c0eb4c73ec8fe68135a7251b24ce65df2027a4a0
xiaofd <568032134@qq.com>
2018-09-01 15:27:37 +0800

    Update loc.py

--- loc.py
+++ loc.py
@@ -12,12 +12,12 @@ headers={
     'Host': 'www.hostloc.com',
     'Connection': 'keep-alive',
     'Cache-Control': 'max-age=0',
-    'Origin': 'http://www.hostloc.com',
+    'Origin': 'https://www.hostloc.com',
     'Upgrade-Insecure-Requests': '1',
     'Content-Type': 'application/x-www-form-urlencoded',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Referer': 'http://www.hostloc.com/forum.php',
+    'Referer': 'https://www.hostloc.com/forum.php',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-HK,zh;q=0.9,zh-CN;q=0.8,en-US;q=0.7,en;q=0.6,zh-TW;q=0.5'
 }
@@ -25,6 +25,7 @@ data={
     'fastloginfield':'username',
     'username':'',
     'password':'',
+    'cookietime':'2592000',
     'quickforward':'yes',
     'handlekey':'ls'
 }
@@ -33,23 +34,23 @@ import sys
 data['username']=sys.argv[1]
 data['password']=sys.argv[2]
 
-sess.post('http://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1',headers=headers,data=data)
+sess.post('https://www.hostloc.com/member.php?mod=logging&action=login&loginsubmit=yes&infloat=yes&lssubmit=yes&inajax=1',headers=headers,data=data)
 
 import random
 import re
-bscore=re.findall('积分: ([0-9]+)',sess.get('http://www.hostloc.com/forum.php').content.decode('utf-8'))[0]
+bscore=re.findall('积分: ([0-9]+)',sess.get('https://www.hostloc.com/forum.php').content.decode('utf-8'))[0]
 print('Hostloc签到前积分: ',bscore)
 
 # [ sess.get('http://www.hostloc.com/space-uid-{}.html'.format(random.randint(10000,20000))) for i in range(15) ]
 
 from multiprocessing.dummy import Pool as ThreadPool
 pool = ThreadPool(10) # 10个线程
-results = pool.map(lambda x: sess.get(x), [ 'http://www.hostloc.com/space-uid-{}.html'.format(random.randint(10000,20000)) for i in range(20) ]) # urls是任务列表 list，第一个参数是线程函数
+results = pool.map(lambda x: sess.get(x), [ 'https://www.hostloc.com/space-uid-{}.html'.format(random.randint(10000,20000)) for i in range(20) ]) # urls是任务列表 list，第一个参数是线程函数
 # close the pool and wait for the work to finish
 pool.close()
 pool.join()
 
-ascore=re.findall('积分: ([0-9]+)',sess.get('http://www.hostloc.com/forum.php').content.decode('utf-8'))[0]
+ascore=re.findall('积分: ([0-9]+)',sess.get('https://www.hostloc.com/forum.php').content.decode('utf-8'))[0]
 print('Hostloc签到后积分: ',ascore)
 
 from termcolor import *
